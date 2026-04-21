97a2b577042574a2cadf0b1f05f93e3cd9387991
iann <ianathanwong@gmail.com>
2026-04-21 13:21:24 -0700

    Player Movement Implemented

---
+++ Project 3.2/Assets/Scripts/2_Player/Player.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+public class Player : MonoBehaviour
+{
+    public static Player Instance { get; private set; }
+
+    [SerializeField] private PlayerMovement playerMovement;
+    [SerializeField] private LayerMask groundLayer;
+
+    // Player Input
+    private PlayerInput _input;
+    private bool _inputEnabled;
+
+    private Vector3 _mousePosition;
+
+    // Singleton Initialization
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Component Initialization
+    void Start()
+    {
+        // Player Input 
+        _input = new PlayerInput();
+        _input.Enable();
+        _inputEnabled = true;
+
+        // Player Components
+        playerMovement.Initialize();
+    }
+
+    void Update()
+    {
+        // Record Mouse Position in World Space
+        Ray cursorPosition = Camera.main.ScreenPointToRay(_input.General.Mouse.ReadValue<Vector2>());
+        if (Physics.Raycast(cursorPosition, out RaycastHit hit, Mathf.Infinity, groundLayer)) {
+            _mousePosition = hit.point;
+        }
+
+        // Movement Input
+        var input = _input.General;
+        var movementInput = new MovementInput
+        {
+            Movement        = _inputEnabled ? input.Move.ReadValue<Vector2>() : Vector2.zero,
+            Roll           = _inputEnabled && input.Roll.WasPressedThisFrame(),
+            MousePosition   = _inputEnabled ? _mousePosition : Vector3.zero
+        };
+        playerMovement.UpdateInput(movementInput);
+    }
+
+    /// <summary>
+    /// * Updates...
+    ///     - Player Rotation
+    /// </summary>
+    void LateUpdate()
+    {
+        var deltaTime = Time.deltaTime;
+
+        playerMovement.UpdateRotation(deltaTime);
+    }
+
+    // Updates Player Movement
+    void FixedUpdate()
+    {
+        var fixedDeltaTime = Time.fixedDeltaTime;
+        playerMovement.UpdateMovement(fixedDeltaTime);
+    }
+
+    void OnDisable() => _input.Dispose();
+}
---
+++ Project 3.2/Assets/Scripts/2_Player/PlayerMovement.cs
@@ -0,0 +1,197 @@
+using UnityEngine;
+
+public struct MovementState
+{
+    public MovementAction CurrentAction;
+    public Vector3 Velocity;
+    public bool IsGrounded;
+}
+public enum MovementAction
+{
+    Idle    = 0,
+    Move    = 1,
+    Roll    = 2
+}
+public struct MovementInput
+{
+    public Vector2 Movement;
+    public Vector3 MousePosition;
+    public bool Roll;
+}
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float moveAcceleration = 20f;
+    [SerializeField] private float moveRotation = 15f;
+
+    [Header("Roll")]
+    [SerializeField] private CapsuleCollider hurtbox;
+    [SerializeField] private float rollSpeed = 12;
+    [SerializeField] private float rollAcceleration = 20f;
+    [SerializeField] private float rollDuration = 0.5f;
+
+    private CharacterController _controller;
+    private bool _movementInputEnabled;
+
+    // Requested Inputs
+    private Vector3 _requestedMovement;
+    private Vector3 _requestedMousePosition;
+    private bool _requestedRoll;
+
+    // State Machine
+    private MovementState _state;
+    private MovementState _prevState;
+
+    // Roll Variables
+    private struct RollData
+    {
+        public Vector3 Direction;
+        public bool Triggered;
+        public float Timer;
+    }
+    private RollData _rollData;
+
+
+    // Start()
+    public void Initialize()
+    {
+        _controller = GetComponent<CharacterController>();
+
+        _movementInputEnabled = true;
+
+        _state.CurrentAction = MovementAction.Idle;
+        _state.Velocity = Vector3.zero;
+        _state.IsGrounded = _controller.isGrounded;
+        _prevState = _state;
+    }
+
+    // Update()
+    public void UpdateInput(MovementInput input)
+    {
+        if (_movementInputEnabled)
+        {
+            _requestedMovement = new Vector3(input.Movement.x, 0f, input.Movement.y).normalized;
+
+            _requestedRoll = input.Roll;
+            TryTriggerRoll();
+
+            _requestedMousePosition = input.MousePosition;
+        }
+    }
+
+    // FixedUpdate()
+    public void UpdateMovement(float fixedDeltaTime)
+    {
+        ApplyGravity();
+
+        // Roll Movement 
+        // * roll triggered *
+        if (_rollData.Triggered)
+        {
+            _state.CurrentAction = MovementAction.Roll;
+            _rollData.Timer += fixedDeltaTime;
+
+            // Sustain dodge velocity during duration
+            var targetVelocity = rollSpeed * _rollData.Direction;
+            _state.Velocity = Vector3.Lerp
+            (
+                _state.Velocity,
+                targetVelocity,
+                1f - Mathf.Exp(-rollAcceleration * fixedDeltaTime)
+            );
+
+            // Reset everything once dodge duration reached
+            if (_rollData.Timer > rollDuration)
+            {
+                _rollData.Triggered = false;
+                _movementInputEnabled = true;
+                hurtbox.enabled = true;
+            }
+        }
+        // Regular Movement
+        // * movement input *
+        else if (_requestedMovement.sqrMagnitude > 0f && _movementInputEnabled)
+        {
+            _state.CurrentAction = MovementAction.Move;
+
+            var targetVelocity = moveSpeed * _requestedMovement;
+            _state.Velocity = Vector3.Lerp
+            (
+                _state.Velocity,
+                targetVelocity,
+                1f - Mathf.Exp(-moveAcceleration * fixedDeltaTime)
+            );
+        }
+        // Idle
+        // * no input *
+        else if (_requestedMovement.sqrMagnitude == 0f && _movementInputEnabled)
+        {
+            _state.CurrentAction = MovementAction.Idle;
+            _state.Velocity = Vector3.Lerp
+            (
+                _state.Velocity,
+                Vector3.zero,
+                1f - Mathf.Exp(-moveAcceleration * fixedDeltaTime)
+            );
+        }
+        
+
+        // Apply Movement
+        if (_controller.enabled) _controller.Move(_state.Velocity * fixedDeltaTime);
+
+        // Update State Machine
+        _prevState = _state;
+    }
+
+    // LateUpdate()
+    public void UpdateRotation(float deltaTime)
+    {
+        // Rotate towards direction of movement
+        if (_requestedMovement.sqrMagnitude > 0f)
+        {
+            var targetRotation = Quaternion.LookRotation(_requestedMovement);
+            transform.rotation = Quaternion.Lerp
+            (
+                transform.rotation,
+                targetRotation,
+                1f - Mathf.Exp(-moveRotation * deltaTime)
+            );
+        }
+    }
+
+
+    #region *--- Helper Functions --------------------------------------------------*
+    private void ApplyGravity()
+    {
+        // Gravity
+        _state.IsGrounded = _controller.isGrounded;
+        if (_state.IsGrounded)
+        {
+            if (_prevState.Velocity.y < -2f) {
+                _state.Velocity.y = -2f;
+            }
+        } else {
+            _state.Velocity += 2 * Time.deltaTime * Physics.gravity;
+        }
+    }
+    private void TryTriggerRoll()
+    {
+        if (_requestedRoll && !_rollData.Triggered && _requestedMovement.sqrMagnitude > 0f)
+        {
+            // Update dodge info
+            _rollData.Triggered = true;
+            _rollData.Direction = _requestedMovement;
+            _rollData.Timer = 0f;
+
+            // Disable hurtbox
+            hurtbox.enabled = false;
+
+            // Disable player inputs
+            _movementInputEnabled = false;
+        }
+    }
+    #endregion
+}
