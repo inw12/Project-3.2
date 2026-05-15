74719a8166ec2c72105799183eee36af8cdb7841
iann <ianathanwong@gmail.com>
2026-05-15 13:23:32 -0700

    Added Animator Parameter variables to EnemyAnimationController

--- Project 3.2/Assets/Scripts/2_Enemy/EnemyAnimationController.cs
+++ Project 3.2/Assets/Scripts/2_Enemy/EnemyAnimationController.cs
@@ -4,6 +4,18 @@ public class EnemyAnimationController : MonoBehaviour
 {
     private Animator _animator;
 
+    // Animator Parameters
+    private static readonly int CurrentAction       = Animator.StringToHash("CurrentAction");
+    private static readonly int AttackID            = Animator.StringToHash("AttackID");
+    private static readonly int AttackTrigger       = Animator.StringToHash("AttackTrigger");    
+    private static readonly int AttackActive        = Animator.StringToHash("AttackActive");    
+    private static readonly int ComboTrigger        = Animator.StringToHash("ComboTrigger");
+    private static readonly int KnockbackTrigger    = Animator.StringToHash("KnockbackTrigger");
+    private static readonly int InHitstun           = Animator.StringToHash("InHitstun");
+    private static readonly int PullTrigger         = Animator.StringToHash("PullTrigger");
+    private static readonly int PlayerPulled        = Animator.StringToHash("PlayerPulled");
+    private static readonly int InParryPhase        = Animator.StringToHash("InParryPhase");
+
     public void Initialize()
     {
         _animator = GetComponent<Animator>();
