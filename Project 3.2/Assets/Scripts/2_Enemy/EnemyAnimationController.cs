32fe6f816f451dc879670ab2ebc0b446e99cee0c
iann <ianathanwong@gmail.com>
2026-05-29 13:15:52 -0700

    Removed "AttackTrigger" Animation Parameter

--- Project 3.2/Assets/Scripts/2_Enemy/EnemyAnimationController.cs
+++ Project 3.2/Assets/Scripts/2_Enemy/EnemyAnimationController.cs
@@ -16,8 +16,7 @@ public class EnemyAnimationController : MonoBehaviour
     // * "High Level" Parameters
     //      - Parameters used by Enemy scripts
     private static readonly int CurrentAction       = Animator.StringToHash("CurrentAction");
-    private static readonly int AttackID            = Animator.StringToHash("AttackID");
-    private static readonly int AttackTrigger       = Animator.StringToHash("AttackTrigger");    
+    private static readonly int AttackID            = Animator.StringToHash("AttackID");  
     private static readonly int AttackActive        = Animator.StringToHash("AttackActive");    
     private static readonly int KnockbackTrigger    = Animator.StringToHash("KnockbackTrigger");
     private static readonly int InHitstun           = Animator.StringToHash("InHitstun");
