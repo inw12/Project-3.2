e27f33a0fb141eecd68165d01cc76dc1b25ddfd8
iann <ianathanwong@gmail.com>
2026-06-29 19:13:05 -0700

    New AttackIndicator
    New Attack Indicator object for FocusShot

---
+++ Project 3.2/Assets/Scripts/1_Resources/VFX/LaserIndicator.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class LaserIndicator : MonoBehaviour
+{
+    [SerializeField] private LineRenderer lineRenderer;
+    [SerializeField] private Transform start;
+    [SerializeField] private Transform end;
+    [Space]
+    [SerializeField] private Color baseColor;
+    [SerializeField] private Color blinkColor;
+    [Space]
+    [SerializeField] private float blinkDuration;
+
+    private MeshRenderer _startMesh;
+    private MeshRenderer _endMesh;
+    private MaterialPropertyBlock _mpb;
+
+    private float _timer;
+    private bool _isBlinking;
+
+    private static readonly int BlinkProgress = Shader.PropertyToID("_BlinkProgress");
+
+    void Start()
+    {
+        lineRenderer.SetPosition(0, start.position);
+        lineRenderer.SetPosition(1, end.position);
+
+        _timer = 0f;
+        _isBlinking = true;
+
+        _startMesh = start.GetComponent<MeshRenderer>();
+        _endMesh = end.GetComponent<MeshRenderer>();
+        _mpb = new MaterialPropertyBlock();
+    }
+
+    void Update()
+    {
+        lineRenderer.SetPosition(0, start.position);
+        lineRenderer.SetPosition(1, end.position);
+
+        // "Blinking" Logic
+        _timer = _isBlinking ? _timer += Time.deltaTime : _timer -= Time.deltaTime;
+        var p = Mathf.Clamp01(_timer / blinkDuration);
+        if (p >= 1f) _isBlinking = false;
+        if (p <= 0f) _isBlinking = true;   
+
+        // Update Shader Graph
+        _endMesh.GetPropertyBlock(_mpb);
+        _mpb.SetFloat(BlinkProgress, p);
+        _endMesh.SetPropertyBlock(_mpb);
+
+        _mpb = new MaterialPropertyBlock();
+
+        _startMesh.GetPropertyBlock(_mpb);
+        _mpb.SetFloat(BlinkProgress, p);
+        _startMesh.SetPropertyBlock(_mpb);
+    }
+
+    public void SetStartPosition(Vector3 position)  => start.position = position;
+    public void SetEndPosition(Vector3 position)    => end.position = position;
+}
